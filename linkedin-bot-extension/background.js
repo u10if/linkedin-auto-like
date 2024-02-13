d0ebda0470ec203bebb5a808ba719a05fa1e690f
kiarash <u10if@outlook.com>
2024-02-13 19:59:17 +0330

    Delete linkedin-bot-extension directory

--- linkedin-bot-extension/background.js
+++
@@ -1,8 +0,0 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.command === "getStatus") {
-    chrome.storage.local.get("extensionEnabled", (data) => {
-      sendResponse({status: data.extensionEnabled});
-    });
-    return true; // Keep the message channel open for asynchronous response
-  }
-});
